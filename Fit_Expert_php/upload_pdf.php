59b12475c04379dfa3fb53c9410abd05f99a863a
JuanDiegoVTG <juandiegoguasca0@gmail.com>
2026-06-10 21:11:28 -0500

    update: archivos php y extensiones

--- Fit_Expert_php/upload_pdf.php
+++ Fit_Expert_php/upload_pdf.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Headers: Content-Type");
+require_once __DIR__ . '/vendor/autoload.php';
 
 $javaAppUrl = getenv('JAVA_APP_URL') ?: "http://localhost:8082";
 
